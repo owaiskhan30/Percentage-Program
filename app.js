9a7070625157d143f1999c37dac19d1e819e7446
owaiskhan30 <owaiskhan.raees@gmail.com>
2024-08-29 04:09:00 +0500

    changes

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var obtained_marks = math_marks + urdu_marks + physics_marks + chemistry_marks;
 
 var percentage = (obtained_marks / total_marks) * 100;
 
-console.log(math_marks);
-console.log(urdu_marks);
-console.log(physics_marks);
-console.log(chemistry_marks);
+console.log("Math Marks" + " " + math_marks);
+console.log("Urdu Marks" + " " + urdu_marks);
+console.log("Physics Marks" + " " + physics_marks);
+console.log("Chemistry Marks" + " " + chemistry_marks);
 
 if (math_marks >= 25 || urdu_marks >= 25 || physics_marks >= 25 || chemistry_marks >= 25) {
     console.log("Congratulation You scored" + " " + obtained_marks + "out of" + " " + total_marks + " " + "marks.");
